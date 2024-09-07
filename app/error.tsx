dc46941aff14053b0ca8152882363557eb1ac997
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-09-08 00:33:22 +0530

    Refactor error component and add button for retry functionality

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 "use client"; // Error boundaries must be Client Components
 
+import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
 
 export default function Error({
@@ -15,16 +16,13 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="min-h-screen flex justify-center items-center text-center">
+      <div className="space-y-2">
+        <h2 className="text-2xl font-semibold">Something Went Wrong</h2>
+        <Button variant="secondary" className="rounded-xl mt-2" onClick={reset}>
+          Try Again
+        </Button>
+      </div>
     </div>
   );
 }
