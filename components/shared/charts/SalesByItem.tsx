1039e5c2c338cfe4757acb2fd619f4cdd9c9825c
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-09-08 00:31:49 +0530

    Refactor sales charts components

--- components/shared/charts/SalesByItem.tsx
+++ components/shared/charts/SalesByItem.tsx
@@ -46,37 +46,44 @@ export default function SalesByItem({ period }: { period: string }) {
         <CardDescription>{period}</CardDescription>
       </CardHeader>
       <CardContent>
-        {chartData.length < 1 && <Loader />}
-        <ChartContainer config={chartConfig} className="max-h-[250px] w-full">
-          <BarChart
-            accessibilityLayer
-            data={chartData}
-            margin={{
-              top: 20,
-            }}
-          >
-            <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="name"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-            />
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Bar dataKey="totalSales" fill="var(--color-totalSales)" radius={8}>
-              <LabelList
-                position="top"
-                offset={12}
-                className="fill-foreground"
-                formatter={(value: number) => value.toFixed(1)}
-                fontSize={12}
+        {chartData.length > 0 ? (
+          <ChartContainer config={chartConfig} className="max-h-[250px] w-full">
+            <BarChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                top: 20,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="name"
+                tickLine={false}
+                tickMargin={10}
+                axisLine={false}
               />
-            </Bar>
-          </BarChart>
-        </ChartContainer>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Bar
+                dataKey="totalSales"
+                fill="var(--color-totalSales)"
+                radius={8}
+              >
+                <LabelList
+                  position="top"
+                  offset={12}
+                  className="fill-foreground"
+                  formatter={(value: number) => value.toFixed(1)}
+                  fontSize={12}
+                />
+              </Bar>
+            </BarChart>
+          </ChartContainer>
+        ) : (
+          <Loader />
+        )}
       </CardContent>
       <CardFooter className="flex-col items-start gap-2 text-sm"></CardFooter>
     </Card>
--- components/shared/charts/SalesByPurchaseMethod.tsx
+++ components/shared/charts/SalesByPurchaseMethod.tsx
@@ -65,55 +65,58 @@ export default function SalesByPurchaseMethod({ period }: { period: string }) {
         </CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
-        {chartData.length < 1 && <Loader />}
-        <ChartContainer
-          config={chartConfig}
-          className="mx-auto aspect-square max-h-[250px]"
-        >
-          <PieChart>
-            <ChartTooltip
-              cursor={false}
-              content={<ChartTooltipContent hideLabel />}
-            />
-            <Pie
-              data={chartData}
-              dataKey="sales"
-              nameKey="purchaseMethod"
-              innerRadius={60}
-              strokeWidth={5}
-            >
-              <Label
-                content={({ viewBox }) => {
-                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                    return (
-                      <text
-                        x={viewBox.cx}
-                        y={viewBox.cy}
-                        textAnchor="middle"
-                        dominantBaseline="middle"
-                      >
-                        <tspan
+        {chartData.length > 0 ? (
+          <ChartContainer
+            config={chartConfig}
+            className="mx-auto aspect-square max-h-[250px]"
+          >
+            <PieChart>
+              <ChartTooltip
+                cursor={false}
+                content={<ChartTooltipContent hideLabel />}
+              />
+              <Pie
+                data={chartData}
+                dataKey="sales"
+                nameKey="purchaseMethod"
+                innerRadius={60}
+                strokeWidth={5}
+              >
+                <Label
+                  content={({ viewBox }) => {
+                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                      return (
+                        <text
                           x={viewBox.cx}
                           y={viewBox.cy}
-                          className="fill-foreground text-3xl font-bold"
+                          textAnchor="middle"
+                          dominantBaseline="middle"
                         >
-                          {totalSales.toLocaleString()}
-                        </tspan>
-                        <tspan
-                          x={viewBox.cx}
-                          y={(viewBox.cy || 0) + 24}
-                          className="fill-muted-foreground"
-                        >
-                          Sales
-                        </tspan>
-                      </text>
-                    );
-                  }
-                }}
-              />
-            </Pie>
-          </PieChart>
-        </ChartContainer>
+                          <tspan
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            className="fill-foreground text-3xl font-bold"
+                          >
+                            {totalSales.toLocaleString()}
+                          </tspan>
+                          <tspan
+                            x={viewBox.cx}
+                            y={(viewBox.cy || 0) + 24}
+                            className="fill-muted-foreground"
+                          >
+                            Sales
+                          </tspan>
+                        </text>
+                      );
+                    }
+                  }}
+                />
+              </Pie>
+            </PieChart>
+          </ChartContainer>
+        ) : (
+          <Loader />
+        )}
       </CardContent>
     </Card>
   );
